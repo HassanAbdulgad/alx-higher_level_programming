339e073a3c7502fedf61e9d91b5616b21b044b2a
HassanAbdulgad <hassanabdulgader16@gmail.com>
2024-01-10 02:20:27 -0800

    THIS Done by Hassan

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,17 +1,3 @@
 #!/usr/bi/python3
 for i in range(0, 99):
-    if(i > 9 and i <= 15):
-        if i == 10:
-            print(i, " = 0x", "a")
-        elif i == 11:
-            print(i, " = 0x", "b")
-        elif i == 12:
-            print(i, " = 0x", "c")
-        elif i == 13:
-            print(i, " = 0x", "d")
-        elif i == 14:
-            print(i, " = 0x", "e")
-        elif i == 15:
-            print(i, " = 0x", "f")
-    else:
-        print(i, " = 0x", i)
+    print('{} = 0x{:x}'.format(i, i))
