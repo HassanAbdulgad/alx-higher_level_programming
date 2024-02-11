d35c30434c69862761cd7a9d3b97ccc6571f12ff
HassanAbdulgad <hassanabdulgader16@gmail.com>
2024-02-12 01:10:39 +0200

    SOlved the index problem

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the number of and list of arguments using sys module."""
+    """Print the number of and list of arguments."""
     import sys
 
-    size = len(sys.argv) - 1
-    if size == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif size == 1:
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(size))
-    for i in range(size):
+        print("{} arguments:".format(count))
+    for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
