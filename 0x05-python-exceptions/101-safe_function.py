2e735aa6a81064e4c903988ff1aab8e471234876
HassanAbdulgad <hassanAbdulgader16@gmail.com>
2024-03-23 08:09:30 +0200

    Pycodestyle has been solved

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -16,7 +16,7 @@ def safe_function(fct, *args):
     """
     try:
         result = fct(*args)
-        return result
-    except ValueError as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        return None
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
